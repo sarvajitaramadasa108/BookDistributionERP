bcd627c685177bbfe3f5fc017d96501daf9c49b4
SJRD <sarvajitaramadasa108@gmail.com>
2026-07-02 12:21:41 +0530

    Connect frontend to Apps Script backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,5 @@
 window.ERP_CONFIG = {
   appName: "HKM Visakhapatnam Book Distribution ERP",
-  apiBaseUrl: "",
-  mockMode: true
+  apiBaseUrl: "https://script.google.com/macros/s/AKfycbyY1EB9Yo_3xiWhxoNB5cDZB3G-TewdD99_mKQPOlzFByuV7in1HNV6IRGzjo2tQc2j/exec",
+  mockMode: false
 };
-
