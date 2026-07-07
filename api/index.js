514e2d78b6c431f988856737d438835227f36103
SJRD <sarvajitaramadasa108@gmail.com>
2026-07-07 18:06:22 +0530

    Fix Vercel runtime config

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const SUPABASE_URL = process.env.SUPABASE_URL || "";
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
 
 export const config = {
-  runtime: "nodejs20.x"
+  runtime: "nodejs"
 };
 
 function json(statusCode, data) {
